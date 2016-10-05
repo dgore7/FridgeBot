d41db950b0ae834ca7a736d04dcc16c688807b0e
Kevin Leung <keleung@microsoft.com>
2016-10-05 16:37:13 -0500

    updated luis intent flow

--- fridgeBot/Controllers/MessagesController.cs
+++ fridgeBot/Controllers/MessagesController.cs
@@ -1,102 +1,139 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using System.Net;
+using System.Web.Http;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Http.Description;
+using System.Collections.Generic;
+using fridgeBot.DeserializationJson;
 using Microsoft.Bot.Connector;
-using Newtonsoft.Json;
-using unirest_net.http;
-using System.Text;
+using System.Linq;
 
 namespace fridgeBot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        public string ShoppingList { get; set; }
+
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
+            StateClient stateClient = activity.GetStateClient();
+            BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
+
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+                // holds response to displayto user
+                string answer = "I'm sorry. I didn't understand that. Try: \"What can we cook for dinner?\" or  \"What ingredients are we missing?\"";
 
-                //// return our reply to the user
-                //Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                //await connector.Conversations.ReplyToActivityAsync(reply);
+                Rootobject luisObj = await LUISClient.ParseUserInput(activity.Text);
+                if (luisObj.intents.Length > 0)
+                {
+                    switch (luisObj.intents[0].intent) // assumption: first intent is highest score
+                    {
+                        case "":
+                            answer = "I'm sorry. I didn't understand that. Try: \"What can we cook for dinner?\" or  \"What ingredients are we missing?\"";
+                            break;
 
-                // respond to basic "hello" and "thank you" responses from user
-                // if not, assume the user is passing in a list of ingredients seperated by ", "
+                        case "Greeting":
+                            answer = "Hello! Feeling hungry and adventurous? Try: \"What can we cook for dinner?\" or  \"I'm hungry!\"";
+                            break;
 
-                //if (activity.Text.Contains("hi") || activity.Text.Contains("Hi")
-                //  || activity.Text.Contains("hello") || activity.Text.Contains("Hello"))
-                //{
-                //    Activity replyGreeting = activity.CreateReply($"Hello! If you give me ingredients, I can recommend a recipe for you. (ie. apples,sugar,flower)");
-                //    await connector.Conversations.ReplyToActivityAsync(replyGreeting);
-                //} else if (activity.Text.Contains("thanks") || activity.Text.Contains("Thanks")
-                //    || activity.Text.Contains("thank") || activity.Text.Contains("thanks"))
-                //{
-                //    Activity replyThanks = activity.CreateReply($"You're very welcome! Enjoy your meal!");
-                //    await connector.Conversations.ReplyToActivityAsync(replyThanks);
-                //} else
-                //{
-                //    // remove " " in bewtween ingredients - format for API: spoonacular (GET Find By Ingredients)
-                //    activity.Text = activity.Text.Replace(" ", string.Empty);
-                //    Activity replyAcknowledge = activity.CreateReply($"Let me find recipes for {activity.Text}...");
-                //    await connector.Conversations.ReplyToActivityAsync(replyAcknowledge);
+                        case "FindRecipe":
 
+                            answer = "Could you please provide me with a list of ingredeints you have or wish to use?";
+                            break;
 
-                // take in ingredients list and format correctly to pass into API url
+                        case "PassIngredients":
+                            // take in ingredients list and format correctly to pass into API url
+                            activity.Text.Remove(0, 6);         // remove "I have "
+                            activity.Text.Replace(",", "%2C");  // replace all commas with '%2C'
+                            activity.Text.Replace(" ", "");     // remove all spaces if there any
+                            var argIngredients = activity.Text;
 
-                activity.Text.Replace(",", "%2C");  // replace all commas with '%2C'
-                activity.Text.Replace(" ", "");     // remove all spaces if there any
+                            var caller = new CallAPI();
 
-                string argIngredients = activity.Text;
-
-                // create url by passing in parameters
-                string url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?";
-
-                string param_fillIngredients = "false";
-                string param_ingredients = argIngredients;
-                string param_limitLicense = "false";
-                string param_number = "3";
-                string param_ranking = "1";
-
-                url = url + "fillIngredients=" + param_fillIngredients + "&";
-                url = url + "ingredients=" + param_ingredients + "&";
-                url = url + "limitLicense=" + param_limitLicense + "&";
-                url = url + "number=" + param_number + "&";
-                url = url + "ranking=" + param_ranking;
+                            if (!caller.isLimitHit())
+                            {
+                                // Initialize recipeResult to hold json info
+                                // Call first API to obtain recipe from ingredients
+                                var recipeResult = new JsonRecipe();
+                                recipeResult = caller.GetRecipe(argIngredients);
 
 
-                // "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=5&ranking=1"
-                // These code snippets use an open-source library.
-                HttpResponse<MemoryStream> responseAPI = Unirest.get(url)
-                .header("X-Mashape-Key", "API-KEY")
-                .header("Accept", "application/json")
-                //.field("fillIngredients", "false")
-                //.field("ingredients", "apples,flour,sugar")
-                //.field("limitLicense", "false")
-                //.field("number", "1")
-                //.field("ranking", "1")
-                .asJson<MemoryStream>();
-
-                // Convert MemoryStream into a json array
-                string json = Encoding.Default.GetString(responseAPI.Body.ToArray());
+                                // Initialize recipeResult to hold json info
+                                // Call second API to obtain recipe link from recipeId
+                                var recipeLink = new JsonLink();
+                                recipeLink = caller.GetLink(recipeResult.id.ToString());
 
 
-                // reply back to user with the three recipe options
-                Activity replyRecipe = activity.CreateReply(json);
-                await connector.Conversations.ReplyToActivityAsync(replyRecipe);
+                                // reply back to user with the recipe options
+                                Activity replyMessage = activity;
+                                Activity replyToConversation = replyMessage.CreateReply("May I interest you in..");
+                                replyToConversation.Recipient = replyMessage.From;
+                                replyToConversation.Type = "message";
+                                replyToConversation.Attachments = new List<Attachment>();
+                                List<CardImage> cardImages = new List<CardImage>();
+                                cardImages.Add(new CardImage(url: recipeResult.image));
+                                List<CardAction> cardButtons = new List<CardAction>();
+                                CardAction plButton = new CardAction()
+                                {
+                                    Value = recipeLink.sourceUrl,
+                                    Type = "openUrl",
+                                    Title = "Let's Get Cooking!"
+                                };
+                                cardButtons.Add(plButton);
+                                HeroCard plCard = new HeroCard()
+                                {
+                                    Title = recipeResult.title,
+                                    Subtitle = "Recommended by " + recipeResult.likes.ToString() + " others!",
+                                    Images = cardImages,
+                                    Buttons = cardButtons
+                                };
+                                Attachment plAttachment = plCard.ToAttachment();
+                                replyToConversation.Attachments.Add(plAttachment);
+                                var replyCard = await connector.Conversations.SendToConversationAsync(replyToConversation);
 
-                //}
+                                //GetIngredients
+                                var dict = new SortedDictionary<string, string>();
+                                foreach (var item in recipeLink.extendedIngredients)
+                                {
+                                    dict.Add(item.name, item.aisle);
+                                }
+
+                                ShoppingList = "Shopping List: \n";
+                                foreach (KeyValuePair<string, string> item in dict.OrderBy(key => key.Value))
+                                {
+                                    ShoppingList += String.Format("{0} ({1}).\n", item.Key , item.Value);
+                                }
+                                
+                                // Store the shoppingList string in the current client state
+                                // Allows the string to be accessed in preceeding reply
+                                userData.SetProperty<string>("ShoppingList", ShoppingList);
+                                await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+                            }
+                            answer = "It looks like you may be missing some ingredients! Try: \"Send me a grocery list!\"";
+                            break;
+
+                        case "GetIngredients":
+                            // Retrieve ShoppingList from state              
+                            answer = userData.GetProperty<string>("ShoppingList");
+                            break;
+                    }
+                }
+                else
+                {
+                    //run out of calls.. try again tomorrow... order out?..pizza #
+                    answer = "Sorry! Kitchen is closed.. Time to order out! (425) 453-7200 [Domino's Pizza Bellevue Square]";
+                }
+
+                // Return response back to the user
+                Activity reply = activity.CreateReply(answer);
+                await connector.Conversations.ReplyToActivityAsync(reply);
 
             }
             else
---
+++ fridgeBot/Controllers/MessagesControllerBackup.cs
@@ -0,0 +1,177 @@
+﻿//using System;
+//using System.Net;
+//using System.Web.Http;
+//using System.Net.Http;
+//using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using fridgeBot.DeserializationJson;
+
+//using Microsoft.Bot.Connector;
+//using Microsoft.Bot.Builder.Luis;
+//using Microsoft.Bot.Builder.Dialogs;
+//using Microsoft.Bot.Builder.Luis.Models;
+
+//namespace fridgeBot
+//{
+//    [LuisModel("5ad69a7d-f722-4319-bc73-dbb89d65135f", "8226286cafeb4514963d9228ea3cc6bb")]
+//    [Serializable]
+//    public class FridgeDialog : LuisDialog<object>
+//    {
+//        [LuisIntent("")]
+//        public async Task None(IDialogContext context, LuisResult result)
+//        {
+//            string message = $"I'm sorry. I didn't understand that. Try: \"What can we cook for dinner?\" or  \"What ingredients are we missing?\"";
+//            await context.PostAsync(message);
+//            context.Wait(MessageReceived);
+//        }
+
+//        [LuisIntent("Greeting")]
+//        public async Task Greeting(IDialogContext context, LuisResult result)
+//        {
+//            string message = $"Hello! Feeling hungry and adventurous? Try: \"What can we cook for dinner?\" or  \"I'm hungry!\"";
+//            await context.PostAsync(message);
+//            context.Wait(MessageReceived);
+//        }
+
+//        [LuisIntent("FindRecipe")]
+//        public async Task FindRecipe(IDialogContext context, LuisResult result)
+//        {
+//            string message = $"FindIngredients";
+//            await context.PostAsync(message);
+//            context.Wait(MessageReceived);
+//        }
+
+//        [LuisIntent("GetIngredients")]
+//        public async Task GetIngredients(IDialogContext context, LuisResult result)
+//        {
+//            string message = $"GetIngredients";
+//            await context.PostAsync(message);
+//            context.Wait(MessageReceived);
+//        }
+//    }
+
+//    [BotAuthentication]
+//    public class MessagesController : ApiController
+//    {
+//        /// <summary>
+//        /// POST: api/Messages
+//        /// Receive a message from a user and reply to it
+//        /// </summary>
+//        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+//        {
+//            // check if activity is of type message
+//            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+//            {
+//                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+//                activity.Text.Replace(",", "%2C");  // replace all commas with '%2C'
+//                activity.Text.Replace(" ", "");     // remove all spaces if there any
+//                var argIngredients = activity.Text;
+
+//                await Conversation.SendAsync(activity, () => new FridgeDialog());
+//            }
+//            else
+//            {
+//                HandleSystemMessage(activity);
+//            }
+//            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+
+
+//            if (activity.Type == ActivityTypes.Message)
+//            {
+//                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+//                // take in ingredients list and format correctly to pass into API url
+//                activity.Text.Replace(",", "%2C");  // replace all commas with '%2C'
+//                activity.Text.Replace(" ", "");     // remove all spaces if there any
+//                var argIngredients = activity.Text;
+
+//                var Caller = new CallAPI();
+
+//                if (!Caller.isLimitHit())
+//                {
+//                    // Initialize recipeResult to hold json info
+//                    // Call first API to obtain recipe from ingredients
+//                    var recipeResult = new JsonRecipe();
+//                    recipeResult = Caller.GetRecipe(argIngredients);
+
+
+//                    // Initialize recipeResult to hold json info
+//                    // Call second API to obtain recipe link from recipeId
+//                    var recipeLink = new JsonLink();
+//                    recipeLink = Caller.GetLink(recipeResult.id.ToString());
+
+
+//                    // reply back to user with the recipe options
+//                    Activity replyMessage = activity;
+//                    Activity replyToConversation = replyMessage.CreateReply("May I interest you in..");
+//                    replyToConversation.Recipient = replyMessage.From;
+//                    replyToConversation.Type = "message";
+//                    replyToConversation.Attachments = new List<Attachment>();
+//                    List<CardImage> cardImages = new List<CardImage>();
+//                    cardImages.Add(new CardImage(url: recipeResult.image));
+//                    List<CardAction> cardButtons = new List<CardAction>();
+//                    CardAction plButton = new CardAction()
+//                    {
+//                        Value = recipeLink.sourceUrl,
+//                        Type = "openUrl",
+//                        Title = "Let's Get Cooking!"
+//                    };
+//                    cardButtons.Add(plButton);
+//                    HeroCard plCard = new HeroCard()
+//                    {
+//                        Title = recipeResult.title,
+//                        Subtitle = "Recommended by " + recipeResult.likes.ToString() + " others!",
+//                        Images = cardImages,
+//                        Buttons = cardButtons
+//                    };
+
+//                    Attachment plAttachment = plCard.ToAttachment();
+//                    replyToConversation.Attachments.Add(plAttachment);
+//                    var reply = await connector.Conversations.SendToConversationAsync(replyToConversation);
+//                }
+//                else
+//                {
+//                    //run out of calls.. try again tomorrow... order out?..pizza #
+//                    Activity replyMessage = activity;
+//                    Activity replyToConversation = replyMessage.CreateReply("Sorry! Kitchen is closed.. Time to order out? :)");
+//                    var reply = await connector.Conversations.SendToConversationAsync(replyToConversation);
+//                }               
+//            }
+//            else
+//            {
+//                HandleSystemMessage(activity);
+//            }
+//            var response = Request.CreateResponse(HttpStatusCode.OK);
+//            return response;
+//        }
+
+//        private Activity HandleSystemMessage(Activity message)
+//        {
+//            if (message.Type == ActivityTypes.DeleteUserData)
+//            {
+//                // Implement user deletion here
+//                // If we handle user deletion, return a real message
+//            }
+//            else if (message.Type == ActivityTypes.ConversationUpdate)
+//            {
+//                // Handle conversation state changes, like members being added and removed
+//                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+//                // Not available in all channels
+//            }
+//            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+//            {
+//                // Handle add/remove from contact lists
+//                // Activity.From + Activity.Action represent what happened
+//            }
+//            else if (message.Type == ActivityTypes.Typing)
+//            {
+//                // Handle knowing tha the user is typing
+//            }
+//            else if (message.Type == ActivityTypes.Ping)
+//            {
+//            }
+
+//            return null;
+//        }
+//    }
+//}
